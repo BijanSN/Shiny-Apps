3fdfa1ef2e78d73a8d26d726cd8dda5c9b81c11a
BijanSN <bijan.sadat@gmail.com>
2021-03-02 01:10:38 +0100

    Update Shiny_apps.R

--- Shiny_apps.R
+++ Shiny_apps.R
@@ -6,52 +6,87 @@ library(shiny)
 library(dplyr)
 library(shinydashboard)
 library(ggplot2)
+library(PortfolioAnalytics)
+library(quantmod)
+library(PerformanceAnalytics)
+library(zoo)
+library(ROI)
+library(ggplot2)
+library(reshape2)
+library(scales)
+library(magrittr)
 
-#---- UI ----
+# source("Function1.R")
 
-ui<-dashboardPage(skin = "red",
+#---- Load data -------------------------------------------
+
+# 3 random stocks : Nvidia, Apple and Visa
+getSymbols(c("NVDA", "AAPL", "V"))
+# Get adjusted prices
+prices.data <- merge.zoo(NVDA[,6], AAPL[,6], V[,6])
+
+# Calculate returns
+returns.data <- CalculateReturns(prices.data)
+returns.data <- na.omit(returns.data)
+returns.data <- fortify.zoo(returns.data)
+
+returns.data %<>% 
+  rename(
+    NVDA = NVDA.Adjusted,
+    AAPL = AAPL.Adjusted,
+    V = V.Adjusted
+  )
+
+# or :
+# data(EuStockMarkets)
+# 
+# Dax= EuStockMarkets[,"DAX"]
+# Smi= EuStockMarkets[,"SMI"]
+# Cac= EuStockMarkets[,"CAC"]
+
+#---- UI --------------------------------------------------
+
+ui<-dashboardPage(skin= "red",
   dashboardHeader(title= "Dashboard"),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Iris", tabName = "iris", icon = icon("tree")),
-      menuItem("Cars", tabName = "cars", icon = icon("car"))
+      menuItem("Stocks", tabName = "stocks", icon = icon("signal"))
     )
   ),
   dashboardBody(
     tabItems(
-      #Iris Page
-      tabItem("iris",
-        box(plotOutput("corrolation_plot"),width=8),
+      #Stocks Page
+      tabItem("stocks",
+        box(plotOutput("hist_plot"),width=8),
         box(
-          selectInput("features","Features :",
-                      c("Sepal.Width","Petal.Length",
-                        "Petal.Width")),width = 4
-        )
-      ),
-      #Cars Page
-      tabItem("cars",
-              fluidPage(
-                h1("Cars")
-              )
+          selectInput("stocks","Stocks :",
+                         c("NVDA","AAPL",
+                           "V")),width = 4
+        ),
+        box(plotOutput("density_plot"),width=8)
       )
     )
   )
 )
 
-#------Server -----
+#------Server ----------------------------------------------
 
 server <- function(input,output){
-  output$corrolation_plot <- renderPlot({
-    plot(iris$Sepal.Length,iris[[input$features]],
-    xlab="Sepal length", ylab ="Features")
+
+  # render Histogram 
+  output$hist_plot <-renderPlot({
+    hist(returns.data[[input$stocks]])
+    # plot(returns.data[[input$stocks]])
+  })
+  
+  # render Density
+  output$density_plot <-renderPlot({
+    plot(density(as.numeric(returns.data[[input$stocks]]), adjust = 1.0),col='blue')
   })
 }
 
 #---- Shiny  App -------
 
 shinyApp(ui, server)
-
-
-
 
 
